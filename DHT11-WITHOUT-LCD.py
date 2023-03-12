b69577569c250cbb23594ef57b3e1c20bd3bb0f4
gongcy <gongcy126@126.com>
2023-03-12 17:00:18 +0800

    fmt

--- DHT11-WITHOUT-LCD.py
+++ DHT11-WITHOUT-LCD.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python
-import sys
-import Adafruit_DHT
+import json
+import os
 import time
-import json,os
 
-def jsonWrite(data,tdate,name):
-    pathfile = "web/data/"+ name + "/" + tdate +".json"
+import Adafruit_DHT
+
+
+def jsonWrite(data, tdate, name):
+    pathfile = "web/data/" + name + "/" + tdate + ".json"
     if os.path.exists(pathfile):
-      file =open(pathfile,"rb+")
-      file.seek(-1,os.SEEK_END)
-      file.truncate()
-      file.write(",")
-      json.dump(data,file)
-      file.write("]")
-      file.close()
+        file = open(pathfile, "rb+")
+        file.seek(-1, os.SEEK_END)
+        file.truncate()
+        file.write(",")
+        json.dump(data, file)
+        file.write("]")
+        file.close()
     else:
-      file =open(pathfile,"w")
-      file.write("[")
-      json.dump(data,file)
-      file.write("]")
-      file.close()
+        file = open(pathfile, "w")
+        file.write("[")
+        json.dump(data, file)
+        file.write("]")
+        file.close()
+
+
 timecount = 0
 while True:
     sensor = Adafruit_DHT.DHT11
     humidity, temperature = Adafruit_DHT.read_retry(sensor, 26)
     if humidity is not None and temperature is not None:
-        todaytime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-        msg =  time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time())) + '\n' + str(temperature) + ' C  ' + str(humidity) + '%'
-        i = {"time":time.strftime('%H:%M',time.localtime(time.time())),"tmp":temperature,"hmt":humidity}
-        jsonWrite(i,todaytime,"min")
+        todaytime = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        msg = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time())) + '\n' + str(temperature) + ' C  ' + str(
+            humidity) + '%'
+        i = {"time": time.strftime('%H:%M', time.localtime(time.time())), "tmp": temperature, "hmt": humidity}
+        jsonWrite(i, todaytime, "min")
         print(msg)
-    if(timecount >=60):
+    if (timecount >= 60):
         timecount = 0
-        jsonWrite(i,todaytime,"hour")
+        jsonWrite(i, todaytime, "hour")
     time.sleep(60)
-    timecount = timecount +1
+    timecount = timecount + 1
--- DHT22-WITHOUT-LCD.py
+++ DHT22-WITHOUT-LCD.py
@@ -1,37 +1,43 @@
 #!/usr/bin/python
-import sys
-import Adafruit_DHT
+import json
+import os
 import time
-import json,os
 
-def jsonWrite(data,tdate,name):
-    pathfile = "web/data/"+ name + "/" + tdate +".json"
+import Adafruit_DHT
+
+
+def jsonWrite(data, tdate, name):
+    pathfile = "web/data/" + name + "/" + tdate + ".json"
     if os.path.exists(pathfile):
-      file =open(pathfile,"rb+")
-      file.seek(-1,os.SEEK_END)
-      file.truncate()
-      file.write(",")
-      json.dump(data,file)
-      file.write("]")
-      file.close()
+        file = open(pathfile, "rb+")
+        file.seek(-1, os.SEEK_END)
+        file.truncate()
+        file.write(",")
+        json.dump(data, file)
+        file.write("]")
+        file.close()
     else:
-      file =open(pathfile,"w")
-      file.write("[")
-      json.dump(data,file)
-      file.write("]")
-      file.close()
+        file = open(pathfile, "w")
+        file.write("[")
+        json.dump(data, file)
+        file.write("]")
+        file.close()
+
+
 timecount = 0
 while True:
     sensor = Adafruit_DHT.DHT22
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, 26)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, 4)
     if humidity is not None and temperature is not None:
-        todaytime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-        msg =  time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time())) + '\n' + str(format(temperature,".1f")) + ' C  ' + str(format(humidity,".1f")) + '%'
-        i = {"time":time.strftime('%H:%M',time.localtime(time.time())),"tmp":format(temperature,".1f"),"hmt":format(humidity,".1f")}
-        jsonWrite(i,todaytime,"min")
+        todaytime = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        msg = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time())) + '\n' + str(
+            format(temperature, ".1f")) + ' C  ' + str(format(humidity, ".1f")) + '%'
+        i = {"time": time.strftime('%H:%M', time.localtime(time.time())), "tmp": format(temperature, ".1f"),
+             "hmt": format(humidity, ".1f")}
+        jsonWrite(i, todaytime, "min")
         print(msg)
-    if(timecount >=60):
+    if (timecount >= 60):
         timecount = 0
-        jsonWrite(i,todaytime,"hour")
+        jsonWrite(i, todaytime, "hour")
     time.sleep(60)
-    timecount = timecount +1
+    timecount = timecount + 1
